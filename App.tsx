e8ce08f103cdac40f19606881de020cf08dec35f
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-28 20:29:08 -0300

    feat(ignitegym): add statusbar component

--- App.tsx
+++ App.tsx
@@ -3,13 +3,26 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? (
         <Text style={{ fontFamily: 'Roboto_700Bold' }}>
           Open up App.tsx to start working on your app!
