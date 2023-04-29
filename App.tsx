241d70cb5ac7064a0622b98e328eb4b9d48c728f
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-28 21:00:52 -0300

    feat(ignitegym): add theme

--- App.tsx
+++ App.tsx
@@ -5,28 +5,27 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text } from 'react-native';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      <Loading />
-
-      {/* {fontsLoaded ? (
+      {fontsLoaded ? (
         <Text style={{ fontFamily: 'Roboto_700Bold' }}>
           Open up App.tsx to start working on your app!
         </Text>
       ) : (
-        <View />
-      )} */}
+        <Loading />
+      )}
     </NativeBaseProvider>
   );
 }
