797171a73d1c7a496035018b8179f5078099a00a
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-30 20:54:37 -0300

    feat(ignitegym): add scrollview to take the entire screen when opening the keyboard

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,44 +3,54 @@ import LogoSvg from '@assets/logo.svg';
 
 import { Button, Input } from '@components';
 
-import { Center, Heading, Image, Text, VStack } from 'native-base';
+import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      {}
-      <Image
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        source={BackgroundImg}
-        alt="Pessoas treinando"
-        resizeMode="contain"
-        position="absolute"
-      />
-
-      <Center my={24}>
-        <LogoSvg />
-
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-          Acesse sua conta
-        </Heading>
-
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
         />
-        <Input placeholder="Senha" secureTextEntry />
 
-        <Button title="Acessar" />
-      </Center>
+        <Center my={24}>
+          <LogoSvg />
 
-      <Button title="Criar conta" variant="outline" />
-    </VStack>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center flex={1}>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+
+          <Button title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
