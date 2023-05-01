7143381cdd459cc56cc081ee3a3a20b515fee253
Yuri Cavalini <yuricavalini@gmail.com>
2023-05-01 15:48:05 -0300

    feat(ignitegym): add initial structure for private screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,5 @@
+export * from '../screens/Exercise';
+export * from '../screens/History';
+export * from '../screens/Profile';
 export * from '../screens/SignIn';
 export * from '../screens/SignUp';
