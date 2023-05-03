c260a2abd6fad6703a4d847b25e61e25b7aa4eb6
Yuri Cavalini <yuricavalini@gmail.com>
2023-05-02 21:14:47 -0300

    feat(ignitegym): add group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,26 @@
-import { HomeHeader } from '@components';
-import { VStack } from 'native-base';
+import { Group, HomeHeader } from '@components';
+import { HStack, VStack } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group
+          name="costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+        <Group
+          name="ombros"
+          isActive={groupSelected === 'ombros'}
+          onPress={() => setGroupSelected('ombros')}
+        />
+      </HStack>
     </VStack>
   );
 }
