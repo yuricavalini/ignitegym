210264409cd2d43c30b080df446f02af8aa155e7
Yuri Cavalini <yuricavalini@gmail.com>
2023-05-05 20:43:48 -0300

    feat(ignitegym): add header and userphoto to profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,47 @@
-import { Center, Text } from 'native-base';
+import { ScreenHeader, UserPhoto } from '@components';
+import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState<boolean>(false);
+  const PHOTO_SIZE = 33;
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/yuricavalini.png' }}
+              alt="Foto do usuario"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
