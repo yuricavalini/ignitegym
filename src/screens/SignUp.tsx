2c48633959888c876f0f9f52245f2526e75a3f93
Yuri Cavalini <yuricavalini@gmail.com>
2023-05-09 20:01:06 -0300

    feat(ignitegym): add form validation for name and email, also update Input component style to handle invalid status

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -14,7 +14,11 @@ type FormDataProps = {
 };
 
 export function SignUp() {
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
 
   const navigation = useNavigation();
 
@@ -65,13 +69,25 @@ export function SignUp() {
               required: 'Informe o nome.',
             }}
             render={({ field: { onChange, value } }) => (
-              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
             )}
           />
 
           <Controller
             control={control}
             name="email"
+            rules={{
+              required: 'Informe o e-mail.',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido.',
+              },
+            }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder="E-mail"
@@ -79,6 +95,7 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
